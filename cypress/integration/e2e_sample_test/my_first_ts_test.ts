f1028e459fec8a2c124653ca60c231227a6fb4a6
ShiwanthaL <shiwantha.lakmal@nagarro.com>
2022-05-02 17:10:22 +0530

    Dependancy resolve with log4J

--- cypress/integration/e2e_sample_test/my_first_ts_test.ts
+++ cypress/integration/e2e_sample_test/my_first_ts_test.ts
@@ -2,6 +2,11 @@ import { LoginPage } from '../../pages/loginpage';
 
 const loginPage = new LoginPage();
 
+/**
+ * @Test Test Scenarios : As a user i want to login to the demo app
+ * Given valid username and password
+ * Then i can access and check the portal and logout scuessfully
+ */
 it('Orange HRM Login and Logout E2E Test',() => {
     cy.visit('https://opensource-demo.orangehrmlive.com/');
 
